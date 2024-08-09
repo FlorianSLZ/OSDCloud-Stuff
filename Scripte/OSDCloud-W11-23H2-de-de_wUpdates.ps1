5ead321200e81703adfd8d3cfe28ed0b83e20f97
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-09 10:09:17 +0200

    Create OSDCloud-W11-23H2-de-de_wUpdates.ps1

---
+++ Scripte/OSDCloud-W11-23H2-de-de_wUpdates.ps1
@@ -0,0 +1,77 @@
+<#
+.SYNOPSIS
+    OSDCloud Automation Script for Windows Deployment
+
+.DESCRIPTION
+    Automates the deployment of Windows 11 with specified parameters, downloads OOBE scripts, and sets up post-installation tasks.
+
+.NOTES
+    Author: Florian Salzmann | @FlorianSLZ | https://scloud.work
+    Version: 1.0
+    Date: 2024-08-09
+
+    Changelog:
+    - 2024-08-09: 1.0 Initial version
+    
+#>
+    
+#################################################################
+#   [PreOS] Update Module
+#################################################################
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
+
+#################################################################
+#   [OS] Params and Start-OSDCloud
+#################################################################
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "23H2"
+    OSEdition = "Pro"
+    OSLanguage = "de-de"
+    OSLicense = "Retail"
+    ZTI = $true
+    Firmware = $false
+}
+Start-OSDCloud @Params
+
+#################################################################
+#  [PostOS] OOBE CMD Command Line
+#################################################################
+Write-Host -ForegroundColor Green "Downloading and creating script for OOBE phase"
+Invoke-RestMethod   -Uri 'https://raw.githubusercontent.com/FlorianSLZ/OSDCloud-Stuff/main/OOBE/Set-EmbeddedWINKey.ps1' `
+                    -OutFile 'C:\Windows\Setup\scripts\Set-EmbeddedWINKey.ps1' `
+                    -Encoding UTF8 `
+                    -Force
+
+Invoke-RestMethod   -Uri 'https://raw.githubusercontent.com/FlorianSLZ/OSDCloud-Stuff/main/OOBE/Windows-Updates.ps1' `
+                    -OutFile 'C:\Windows\Setup\scripts\Windows-Updates.ps1' `
+                    -Encoding UTF8 `
+                    -Force
+
+Invoke-RestMethod   -Uri 'https://raw.githubusercontent.com/FlorianSLZ/OSDCloud-Stuff/main/OOBE/OSDCloud-CleanUp.ps1' `
+                    -OutFile 'C:\Windows\Setup\scripts\OSDCloud-CleanUp.ps1' `
+                    -Encoding UTF8 `
+                    -Force
+
+
+$OOBECMD = @'
+@echo off
+# Execute OOBE Tasks
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\Set-EmbeddedWINKey.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\Windows-Updates.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\OSDCloud-CleanUp.ps1
+
+exit 
+'@
+$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
