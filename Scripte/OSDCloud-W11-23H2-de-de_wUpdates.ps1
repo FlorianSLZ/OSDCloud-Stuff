770275a4ecbdfced7d7dec032b1ab1931e83bbec
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-14 13:23:21 +0200

    Update OSDCloud-W11-23H2-de-de_wUpdates.ps1

--- Scripte/OSDCloud-W11-23H2-de-de_wUpdates.ps1
+++ Scripte/OSDCloud-W11-23H2-de-de_wUpdates.ps1
@@ -57,7 +57,7 @@ powershell.exe -Command Get-NetIPAddress
 powershell.exe -Command Test-NetConnection raw.githubusercontent.com -Port 443
 powershell.exe -Command Set-ExecutionPolicy Unrestricted -Force
 
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -File "C:\Windows\Setup\Scripts\Updates-and-Activation.ps1"
+powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -File "C:\Windows\Setup\Scripts\Updates-and-Activation.ps1"
  
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\oobe.cmd' -Encoding ascii -Force
