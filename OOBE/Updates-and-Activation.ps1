beaa208d6534392019b53c27e64ba54a7c732fe9
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-14 13:00:28 +0200

    Update Updates-and-Activation.ps1

--- OOBE/Updates-and-Activation.ps1
+++ OOBE/Updates-and-Activation.ps1
@@ -31,9 +31,10 @@ $Scripts2run = @(
 Write-Host "Starting Windows Updates and Activation"
 Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\Updates-and-Activation.log" -Force | Out-Null
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
 Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module PowerShellGet -RequiredVersion 2.2.4 -SkipPublisherCheck -Force
+
 Install-Script Start-SplashScreen -Force | Out-Null
 Start-SplashScreen.ps1 -Processes $Scripts2run
 
