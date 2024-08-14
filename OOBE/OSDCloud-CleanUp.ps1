0ce4b9e593533a0c93507d54acdfa39df6dbd0f6
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-14 16:55:33 +0200

    Update OSDCloud-CleanUp.ps1

--- OOBE/OSDCloud-CleanUp.ps1
+++ OOBE/OSDCloud-CleanUp.ps1
@@ -1,18 +1,23 @@
-$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Cleanup-Script.log"
-Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
-
-Write-Host "Execute OSD Cloud Cleanup Script" -ForegroundColor Green
+Start-Transcript "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSDCloud-CleanUp.log" | Out-Null
 
 # Copying OSDCloud Logs
 If (Test-Path -Path 'C:\OSDCloud\Logs') {
+    Write-Host "Copying OSDCloud Logs"
     Move-Item 'C:\OSDCloud\Logs\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
 }
 If (Test-Path -Path 'C:\ProgramData\OSDeploy') {
+    Write-Host "Copying OSDCloud Logs"
     Move-Item 'C:\ProgramData\OSDeploy\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
 }
 
 # Cleanup directories
-If (Test-Path -Path 'C:\OSDCloud') { Remove-Item -Path 'C:\OSDCloud' -Recurse -Force }
-If (Test-Path -Path 'C:\Drivers') { Remove-Item 'C:\Drivers' -Recurse -Force }
+If (Test-Path -Path 'C:\OSDCloud') { 
+    Write-Host "Removing OSDCloud directory"
+    Remove-Item -Path 'C:\OSDCloud' -Recurse -Force 
+}
+If (Test-Path -Path 'C:\Drivers') { 
+    Write-Host "Removing Drivers directory"
+    Remove-Item 'C:\Drivers' -Recurse -Force 
+}
 
 Stop-Transcript
