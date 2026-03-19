6948d8eca56f765a6f930a99bf990213d8cd7b58
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2026-03-19 11:27:27 +0100

    Create OSDCloud-GUI.ps1

---
+++ OSDCloud ISO creation/OSDCloud-GUI.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.NOTES
+    Author: Florian Salzmann | @FlorianSLZ | https://scloud.work
+    Version: 1.0
+
+    Changelog:
+    - 2025-02-09: 1.0 Initial version
+    
+#>
+
+
+# Set Workspace Folder
+$ProjectName = "OSDCloud-GUI"
+$WorkspacePath = "C:\OSDCloud\$ProjectName"
+New-Item -ItemType Directory $WorkspacePath -Force | Out-Null
+Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+# Blank Template
+New-OSDCloudTemplate 
+
+
+# Zero Touch
+Edit-OSDCloudWinPE  -WorkspacePath $WorkspacePath `
+                    -StartOSDCloudGUI `
+                    -Brand $ProjectName `
+                    -CloudDriver *
+
+# Save the changes to ISO
+New-OSDCloudISO -WorkspacePath $WorkspacePath
+$FinalISO = "$ProjectName.iso"
+if(Test-Path "$WorkspacePath\$FinalISO"){
+    Remove-Item -Path "$WorkspacePath\$FinalISO" -Force
+}
+
+Rename-Item -Path "$WorkspacePath\OSDCloud_NoPrompt.iso" -NewName "$WorkspacePath\$FinalISO" -Force
+Start-Sleep -s 10
+
+Write-Host -ForegroundColor Green "ISO created: $WorkspacePath\$FinalISO"
